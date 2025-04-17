4e32fd20a91d914b7ac7c0edce64e2cbd885aae8
Sarthak Priyadarshi <sarthakpriyadarshi01@gmail.com>
2025-04-17 13:06:21 +0530

    Added answers logic

---
+++ src/app/api/answer/route.ts
@@ -0,0 +1,95 @@
+import { databases, users } from "@/models/server/config";
+import { db, answerCollection } from "@/models/name";
+import { NextRequest, NextResponse } from "next/server";
+import { ID } from "appwrite";
+import { UserPrefs } from "@/store/Auth";
+
+export async function POST(request: NextRequest) {
+  try {
+    const { questionId, answer, authorId } = await request.json();
+    await databases.createDocument(db, answerCollection, ID.unique(), {
+      content: answer,
+      authodId: authorId,
+      questionId: questionId,
+    });
+
+    // Increase author reputation
+    const prefs = users.getPrefs<UserPrefs>(authorId);
+    await users.updatePrefs<UserPrefs>(authorId, {
+      reputation: Number((await prefs).reputation) + 1,
+    });
+
+    return NextResponse.json({
+      status: 201,
+    });
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error
+        ? error.message
+        : "An error occurred while creating the answer.";
+    const errorStatus =
+      error instanceof Error &&
+      "status" in error &&
+      typeof error.status === "number"
+        ? error.status
+        : error instanceof Error &&
+          "code" in error &&
+          typeof error.code === "number"
+        ? error.code
+        : 500;
+
+    return NextResponse.json(
+      {
+        error: errorMessage,
+      },
+      { status: errorStatus }
+    );
+  }
+}
+
+export async function DELETE(request: NextRequest) {
+  try {
+    const { answerId } = await request.json();
+    const answer = await databases.getDocument(db, answerCollection, answerId);
+    const response = await databases.deleteDocument(
+      db,
+      answerCollection,
+      answerId
+    );
+    const prefs = users.getPrefs<UserPrefs>(answer.authorId);
+    await users.updatePrefs<UserPrefs>(answer.authorId, {
+      reputation: Number((await prefs).reputation) - 1,
+    });
+
+    return NextResponse.json(
+      {
+        data: response,
+      },
+      {
+        status: 205,
+      }
+    );
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error
+        ? error.message
+        : "An error occurred while deleting the answer.";
+    const errorStatus =
+      error instanceof Error &&
+      "status" in error &&
+      typeof error.status === "number"
+        ? error.status
+        : error instanceof Error &&
+          "code" in error &&
+          typeof error.code === "number"
+        ? error.code
+        : 500;
+
+    return NextResponse.json(
+      {
+        error: errorMessage,
+      },
+      { status: errorStatus }
+    );
+  }
+}
